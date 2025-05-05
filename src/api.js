16e8f7a1416d9696ae032f0ea43778a7963ce73b
Piyush Yadav <151051765+piyush72yaduvanshi@users.noreply.github.com>
2025-05-05 19:28:19 +0530

    changes

--- src/api.js
+++ src/api.js
@@ -1,12 +1,9 @@
-// src/api.js
 import axios from 'axios';
 
-const API = axios.create({
-  baseURL: 'https://server-production-646c.up.railway.app/api', // Backend root
-});
+const API = axios.create({ baseURL: 'https://server-production-646c.up.railway.app/api/tasks' });
 
-export const getTasks = (query) => API.get('/tasks', { params: query });
-export const createTask = (data) => API.post('/tasks', data);
-export const updateTask = (id, data) => API.put(`/tasks/${id}`, data);
-export const deleteTask = (id) => API.delete(`/tasks/${id}`);
-export const toggleTask = (id) => API.patch(`/tasks/${id}/toggle-completed`);
+export const getTasks = (query) => API.get(`?${query}`);
+export const createTask = (data) => API.post('/', data);
+export const updateTask = (id, data) => API.put(`/${id}`, data);
+export const deleteTask = (id) => API.delete(`/${id}`);
+export const toggleTask = (id) => API.patch(`/${id}/toggle`);
